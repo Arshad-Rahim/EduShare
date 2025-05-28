5073caeff182c58173d34091e6a6421b35029d61
Arshad Rahim <rahimarshad667@gmail.com>
2025-05-28 20:12:53 +0530

    save data to database

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,28 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { useAppContext } from "./provider/AppProvider";
+import { useSelector } from "react-redux";
 import CallNotification from "./components/videoCall/CallNotification";
+import {Notifications} from "./pages/student/components/Notification";
 import { adminRoutes } from "./routes/AdminRoutes";
 import { publicRoutes } from "./routes/PublicRoutes";
 import { tutorRoutes } from "./routes/TutorRoutes";
 import { userRoutes } from "./routes/UserRoutes";
 
 function App() {
-  const { tutorId } = useAppContext(); // Assuming user contains role and id
+  const user = useSelector((state: any) => state.user.userDatas);
+
+  console.log("App user:", {
+    userId: user?.id || user?._id,
+    userRole: user?.role,
+  });
 
   return (
     <BrowserRouter>
       {/* Render CallNotification for tutors */}
-      {tutorId && <CallNotification tutorId={tutorId} />}
-
+      {user?.role === "tutor" && (user?.id || user?._id) && (
+        <CallNotification tutorId={user.id || user._id} />
+      )}
+      {/* Render Notifications for all authenticated users */}
+      {(user?.id || user?._id) && <Notifications />}
       <Routes>
         {adminRoutes.map((route) => (
           <Route key={route.key} path={route.path} element={route.element} />
@@ -33,4 +42,3 @@ function App() {
 }
 
 export default App;
-
