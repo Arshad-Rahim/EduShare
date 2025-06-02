e283ab7e0bda68bba50494f5e11c5fa106524e28
Arshad Rahim <rahimarshad667@gmail.com>
2025-06-02 18:12:17 +0530

    trying to implement public Axios

--- backend/src/routes/courseRoutes.ts
+++ backend/src/routes/courseRoutes.ts
@@ -61,11 +61,7 @@ export class CourseRoutes {
 
      this.router.get(
        "/all-courses",
-      //  authMiddleware,
-      //  authorizeRole(["user","admin"]),
-      //  checkUserBlocked,
-
-       (req: Request, res: Response) =>
+         (req: Request, res: Response) =>
          injectedCourseController.getAllCourses(req, res)
      );
 
