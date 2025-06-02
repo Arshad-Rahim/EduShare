723519a61d3e6aa9dda07c28181374dfc19a79d7
Arshad Rahim <rahimarshad667@gmail.com>
2025-06-02 20:27:34 +0530

    fixing error in refresh token

--- frontend/src/api/publicAxiosInstance.ts
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-export const publicAxiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_AUTH_BASEURL, 
-  headers: {
-    "Content-Type": "application/json",
-  },
-});
